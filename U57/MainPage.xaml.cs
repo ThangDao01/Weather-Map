90254430d318a0f0aba09bc670b63adb1701c487
ThangDao01 <72856471+ThangDao01@users.noreply.github.com>
2021-10-13 16:49:47 +0700

    Add project files.

---
+++ U57/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace U57
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var location = LocationResult.Text;
+            var myWeather = await OpenWeatherMapProxy.GetWeather(location);
+            string icon = String.Format("https://api.openweathermap.org/img/w/{0}.png",myWeather.weather[0].icon);
+            ResultImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+            ResultTextBlock.Text =    "Country:" + myWeather.sys.country 
+                                    + "\nLocation:" + myWeather.name
+                                    + "\nDescription:" + myWeather.weather[0].description;
+         
+        }
+    }
+}
---
+++ U57/OpenWeatherMapProxy.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
+namespace U57
+{
+    class OpenWeatherMapProxy
+    {
+        public async static Task<RootObject> GetWeather(string q)
+        {
+            var http = new HttpClient();
+            var url = String.Format("https://api.openweathermap.org/data/2.5/weather?q="+q+"&appid=9420349c2c7004a98f7e5370d227d4a9");
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObject)serializer.ReadObject(ms);
+
+            return data;
+        }
+    }
+    [DataContract]
+    public class Coord
+    {
+        [DataMember]
+        public double lon { get; set; }
+
+        [DataMember]
+        public double lat { get; set; }
+     
+    }
+    [DataContract]
+
+    public class Weather
+    {
+        [DataMember]
+
+        public int id { get; set; }
+        [DataMember]
+
+        public string main { get; set; }
+        [DataMember]
+
+        public string description { get; set; }
+
+        [DataMember]
+        public string icon { get; set; }
+    }
+    [DataContract]
+    public class Sys
+    {
+        [DataMember]
+        public string country { get; set; }
+    }
+
+    [DataContract]
+
+    public class RootObject
+    {
+        [DataMember]
+
+        public Coord coord { get; set; }
+
+        [DataMember]
+
+        public List<Weather> weather { get; set; }
+
+
+        [DataMember]
+        public string name { get; set; }
+
+        [DataMember]
+
+        public Sys sys { get; set; }
+
+
+    }
+}
